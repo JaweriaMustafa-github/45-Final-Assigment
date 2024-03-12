455920014b13d34005f1a8e7e4b56727e07ee376
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-03-12 14:37:19 +0500

    first commit

---
+++ Assignment3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = "Life is a Journey,not a Destination";
+let author = "Ralph Waldo Emerson";
+//printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ Assignment3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = "Life is a Journey,not a Destination"
+
+let author:string = "Ralph Waldo Emerson"
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
