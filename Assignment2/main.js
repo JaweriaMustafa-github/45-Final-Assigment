cd1e153a7cc63f09b8ebfaa84ae2d39798fc8eb7
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-03-12 13:25:56 +0500

    first commit

---
+++ Assignment2/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//storing a person's name in a variable
+let personName = "Jaweria Mustafa";
+//printing a person's name in lowercase
+console.log("Lowercase:", personName.toLowerCase());
+//printing a person's name in uppercase
+console.log("Uppercase:", personName.toUpperCase());
+//printing a person's name in titlecase
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1));
---
+++ Assignment2/main.ts
@@ -0,0 +1,15 @@
+//storing a person's name in a variable
+
+let personName:string= "Jaweria Mustafa"
+
+//printing a person's name in lowercase
+
+console.log("Lowercase:", personName.toLowerCase());
+
+//printing a person's name in uppercase
+
+console.log("Uppercase:", personName.toUpperCase());
+
+//printing a person's name in titlecase
+
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1));
