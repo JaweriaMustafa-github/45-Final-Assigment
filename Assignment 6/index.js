99ab714f785b50938575fd9dbd79b2de28d246af
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-04-13 17:15:23 +0500

    first commit

---
+++ Assignment 6/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Assignment 6
+let additionResult = 4 + 4;
+console.log("Addition result:", additionResult);
+let subtractionResult = 10 - 2;
+console.log("Subtraction result:", subtractionResult);
+let multiplicationResult = 4 * 2;
+console.log("Multiplication result:", multiplicationResult);
+let divisionResult = 16 / 2;
+console.log("Division result:", divisionResult);
---
+++ Assignment 6/index.ts
@@ -0,0 +1,12 @@
+// Assignment 6
+let additionResult : number = 4+4;
+console.log("Addition result:", additionResult);
+
+let subtractionResult : number = 10-2;
+console.log("Subtraction result:", subtractionResult);
+
+let multiplicationResult : number = 4*2;
+console.log("Multiplication result:", multiplicationResult);
+
+let divisionResult : number = 16/2;
+console.log("Division result:", divisionResult);
