91f904021d9d8f2977b0a01895cbb3c3e5e4aea5
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-04-13 17:51:12 +0500

    first commit

---
+++ Assignment 9/main.ts
@@ -0,0 +1,13 @@
+// Assignment 9
+// This program adds two numbers and prints the result
+// define two numbers
+
+let num1=10;
+let num2=20;
+
+// Add two numbers
+let result= num1+num2;
+
+//prints result
+console.log(result);
+ 
