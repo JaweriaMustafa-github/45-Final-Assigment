ca08bf000d28a74607f4a452e9c532b05d50fcc8
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-04-13 16:51:35 +0500

    first commit

---
+++ Assignment 5/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//Assignment # 6
+Object.defineProperty(exports, "__esModule", { value: true });
+let nameWithWhiteSpace = " \t \n Jaweria Mustafa \t \n ";
+console.log("Name with white space : ", nameWithWhiteSpace);
+let strippedName = nameWithWhiteSpace.trim();
+console.log("The stripped name is:", strippedName);
---
+++ Assignment 5/index.ts
@@ -0,0 +1,9 @@
+//Assignment # 6
+
+let nameWithWhiteSpace : string = " \t \n Jaweria Mustafa \t \n ";
+console.log("Name with white space : " , nameWithWhiteSpace);
+
+// stripped name
+let strippedName : string = nameWithWhiteSpace.trim();
+console.log("The stripped name is:",strippedName);
+ 
