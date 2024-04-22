1426a9ebd811f6babecc589b5afaf73d086da065
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-04-22 23:20:59 +0500

    first commit

---
+++ Assignment 20/main.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Assignment 20
+let objectContainingItems = {
+    "laptop": {
+        name: "laptop",
+        price: "20000",
+        description: "High performance laptop with fast processor",
+    },
+    "mouse": {
+        name: "mouse",
+        price: "1000",
+        description: "High performance mouse with fast processor",
+    },
+    "keyboard": {
+        name: "laptop",
+        price: "3000",
+        description: "High performance keyboard with fast processor",
+    }
+};
+console.log(objectContainingItems);
---
+++ Assignment 20/main.ts
@@ -0,0 +1,19 @@
+// Assignment 20
+let objectContainingItems : {[key: string] : any} = {
+    "laptop": {
+        name: "laptop",
+        price: "20000",
+        description : "High performance laptop with fast processor",
+    },
+    "mouse": {
+        name: "mouse",
+        price: "1000",
+        description : "High performance mouse with fast processor",
+    },
+    "keyboard": {
+        name: "laptop",
+        price: "3000",
+        description : "High performance keyboard with fast processor",
+    }
+}
+console.log(objectContainingItems);
